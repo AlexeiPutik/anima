04ebe71be98ac44e1bb5784228e8fbc55655b342
Alexey Putik <alexei.putik@gmail.com>
2021-02-18 19:01:05 +0300

    ANIMA-1 Initial commit.

---
+++ ANIMA/ANIMA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ANIMA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
